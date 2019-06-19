d149e0eeb4434d6e7d625c30aa7f0cacbff28adb
Yoko <18836617@qq.com>
2019-06-19 22:36:15 +0800

    binary search

--- dataStructures-algorithm-pra/ch2/main.go
+++ dataStructures-algorithm-pra/ch2/main.go
@@ -2,12 +2,19 @@ package main
 
 import "fmt"
 
+const (
+	NotFound = -1
+)
+
 func main() {
-	a := []int{-1, -2, 3, 4, -1, 1, -3, -4, 5}
-	max := maxSubSequenceSum(a)
-	fmt.Println(max)
+	//a := []int{-1, -2, 3, 4, -1, 1, -3, -4, 5}
+	//max := maxSubSequenceSum(a)
+	//fmt.Println(max)
+	a := []int{1, 2, 3, 4, 5, 6, 10, 34}
+	fmt.Println(binarySearch(a, 10))
 }
 
+// 最大子序列
 func maxSubSequenceSum(a []int) int {
 	thisSum, maxSum := 0, 0
 	for j := 0; j < len(a); j++ {
@@ -19,4 +26,20 @@ func maxSubSequenceSum(a []int) int {
 		}
 	}
 	return maxSum
+}
+
+// 通过二分查找元素下标
+func binarySearch(a []int, target int) int {
+	low, mid, high := 0, 0, len(a) - 1
+	for low <= high {
+		mid = (low + high) / 2
+		if a[mid] < target {
+			low = mid + 1
+		} else if a[mid] > target {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return NotFound
 }
