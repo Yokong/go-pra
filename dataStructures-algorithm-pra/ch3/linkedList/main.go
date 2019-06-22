f4870c159b9622e4ada64542bd0794b4f51ef609
Yoko <18836617@qq.com>
2019-06-22 14:45:00 +0800

    insert remove str for list

--- dataStructures-algorithm-pra/ch3/linkedList/main.go
+++ dataStructures-algorithm-pra/ch3/linkedList/main.go
@@ -1,18 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Item interface{}
 
 type List struct {
-	Item interface{}
+	Elem Item
 	Next *List
 }
 
-func (l *List)IsEmpty() bool {
+func New(item Item) *List {
+	return &List{Elem: item, Next: nil}
+}
+
+func (l *List) IsEmpty() bool {
 	return l.Next == nil
 }
 
+func (l *List) Insert(item Item) {
+	for l.Next != nil {
+		l = l.Next
+	}
+	l.Next = &List{Elem: item}
+}
+
+func (l *List) Remove(item Item) {
+	for l.Next != nil && l.Next.Elem != item {
+		l = l.Next
+	}
+	if l.Next != nil {
+		l.Next = l.Next.Next
+	}
+}
+
+func (l *List) Str() string {
+	var str string
+	for l.Next != nil {
+		str += fmt.Sprint(l.Elem)
+		l = l.Next
+		str += fmt.Sprint("->")
+	}
+	str += fmt.Sprint(l.Elem)
+	return str
+}
+
 func main() {
-	l := List{Item: 5}
-	l.Next = &List{Item: "lala"}
-	fmt.Println(l.IsEmpty())
+	l := &List{Elem: 0}
+	for i := 1; i < 10; i++ {
+		l.Insert(i)
+	}
+	l.Remove(9)
+	fmt.Println(l.Str())
 }
