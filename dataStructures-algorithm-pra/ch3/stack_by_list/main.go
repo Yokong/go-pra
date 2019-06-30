b57fe7f7fad972875201cf32ad91e658458f4ae2
Yoko <18836617@qq.com>
2019-06-30 14:56:23 +0800

    stack by list

---
+++ dataStructures-algorithm-pra/ch3/stack_by_list/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+const (
+	Empty int = -1
+)
+
+type Stack struct {
+	Top int
+	Elem []interface{}
+}
+
+func CreateStack() *Stack {
+	return &Stack{Top: -1}
+}
+
+func (s *Stack) Push(item interface{}) {
+	s.Top++
+	s.Elem = append(s.Elem, item)
+}
+
+func (s *Stack) Pop() interface{} {
+	if s.Top == Empty {
+		return Empty
+	}
+	item := s.Elem[s.Top]
+	s.Elem = s.Elem[:s.Top]
+	s.Top--
+	return item
+}
+
+func main() {
+	s := CreateStack()
+	s.Push(10)
+	s.Push(11)
+	s.Push(12)
+	s.Push(13)
+	s.Push(14)
+	s.Push(15)
+	s.Push(16)
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+}
