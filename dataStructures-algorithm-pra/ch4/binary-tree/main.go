ca9b8eafe8e8465abe893556ec3f726b048f512d
Yoko <18836617@qq.com>
2019-07-03 22:24:05 +0800

    init tree

---
+++ dataStructures-algorithm-pra/ch4/binary-tree/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Elem int
+
+type Tree struct {
+	E Elem
+	Left *Tree
+	Right *Tree
+}
+
+func Find(item Elem, t *Tree) *Tree {
+	if t == nil {
+		return nil
+	}
+	if item < t.E {
+		return Find(item, t.Left)
+	} else if item > t.E {
+		return Find(item, t.Right)
+	} else {
+		return t
+	}
+}
+
+func Insert(item Elem, t *Tree) *Tree {
+	if t == nil {
+		return &Tree{E: item}
+	} else if t.E == 0 {
+		t.E = item
+		return t
+	} else if item < t.E {
+		t.Left = Insert(item, t.Left)
+	} else if item > t.E {
+		t.Right = Insert(item, t.Right)
+	}
+	return t
+}
+
+func CreateTree() *Tree {
+	return &Tree{}
+}
+
+func main() {
+	t := CreateTree()
+	t = Insert(10, t)
+	t = Insert(20, t)
+	t = Insert(5, t)
+	fmt.Println(t)
+}
