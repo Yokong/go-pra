ff97a7c0faf1c1aec05772c8533e2d62c94a7d76
Yoko <18836617@qq.com>
2019-09-02 23:46:24 +0800

    打家劫舍

--- dataStructures-algorithm-pra/dynamic_programming/main.go
+++ dataStructures-algorithm-pra/dynamic_programming/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func intMax(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 // 1. 爬楼梯
 func climbStairs(n int) int {
 	dp := make([]int, n+3)
@@ -13,7 +20,26 @@ func climbStairs(n int) int {
 	return dp[n]
 }
 
+// 2. 打家劫舍
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = intMax(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = intMax(dp[i-1], dp[i-2]+nums[i])
+	}
+	return dp[len(nums)-1]
+}
+
 func main() {
-	r := climbStairs(3)
-	fmt.Println(r)
+	a := []int{5, 2, 6, 3, 1, 7}
+	b := rob(a)
+	fmt.Println(b)
 }
