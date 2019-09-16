1c4e9d69bcf8c076c5ee9e6fa72cc688eb4e6cf9
Yoko <18836617@qq.com>
2019-09-16 22:13:42 +0800

    单方向Channel

--- test-pra/ch-pra/main.go
+++ test-pra/ch-pra/main.go
@@ -32,24 +32,31 @@ func fib2(x int) int {
 	return b
 }
 
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+
 func main() {
 	naturals := make(chan int)
-	squares := make(chan int)
+	s := make(chan int)
 
-	go func() {
-		for x := 0; ; x++ {
-			naturals <- x
-		}
-	}()
-
-	go func() {
-		for {
-			x := <-naturals
-			squares <- x * x
-		}
-	}()
-
-	for {
-		fmt.Println(<-squares)
-	}
+	go counter(naturals)
+	go squarer(s, naturals)
+	printer(s)
 }
