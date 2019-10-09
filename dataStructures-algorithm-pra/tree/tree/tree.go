2de6f5fd8c8ee1c6bfe730807eb2c80ec90458d1
Yoko <18836617@qq.com>
2019-10-09 08:03:12 +0800

    二叉树最大深度

--- dataStructures-algorithm-pra/tree/tree/tree.go
+++ dataStructures-algorithm-pra/tree/tree/tree.go
@@ -6,6 +6,13 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
 // 相同的树
 func IsSameTree(p *TreeNode, q *TreeNode) bool {
 	if p == nil && q == nil {
@@ -33,4 +40,14 @@ func isMirror(t1 *TreeNode, t2 *TreeNode) bool {
 		return false
 	}
 	return (t1.Val == t2.Val) && isMirror(t1.Left, t2.Right) && isMirror(t1.Right, t2.Left)
+}
+
+// 二叉树的最大深度
+func MaxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := MaxDepth(root.Left)
+	right := MaxDepth(root.Right)
+	return max(left, right) + 1
 }
